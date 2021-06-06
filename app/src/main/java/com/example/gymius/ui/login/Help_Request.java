d945312b2c0261a7e28cf23ccf099b83d5809a5e
NeCr00 <36794189+NeCr00@users.noreply.github.com>
2021-06-06 21:37:27 +0300

    Create Trainer class

---
+++ app/src/main/java/com/example/gymius/ui/login/Help_Request.java
@@ -0,0 +1,31 @@
+package com.example.gymius.ui.login;
+
+public class Help_Request {
+
+    private int idHelpRequest, idClient;
+    private int idEquipment;
+
+    public Help_Request(int idClient, int idEquipment) {
+        this.idClient = idClient;
+        this.idEquipment = idEquipment;
+        this.CreateHelpRequest();
+    }
+
+    public void CreateHelpRequest(){
+        //dhmiourgei ena neo help req sto database otan dhmiourghthei ena neo antikeimeno help_request
+        // h vash gyrnaei to id tou neou aithmatos
+        idHelpRequest = 1; //create new help req to database and get the id,
+    }
+
+    public int getIdHelpRequest() {
+        return idHelpRequest;
+    }
+
+    public int getIdClient() {
+        return idClient;
+    }
+
+    public int getIdEquipment() {
+        return idEquipment;
+    }
+}
---
+++ app/src/main/java/com/example/gymius/ui/login/Subscription.java
@@ -0,0 +1,45 @@
+package com.example.gymius.ui.login;
+
+public class Subscription {
+
+    private enum SubType{
+        MHNIAIA, TRIMHNH, EKSAMHNH, ETHSIA
+    }
+    private SubType Subscription_type;
+    private int duration; //hmeres
+    private int id; //
+
+    public Subscription(SubType subscription_type, int duration) {
+        Subscription_type = subscription_type;
+        this.duration = duration;
+    }
+
+    public SubType getSubscription_type() {
+        return Subscription_type;
+    }
+
+    public void setSubscription_type(SubType subscription_type) {
+        Subscription_type = subscription_type;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void createSubDatabase (){
+        // dhmiourgei ena neo entry sto database gia to neo sub
+        //gyrnaei to id tou sub
+    }
+}
