71fa563667ca0f2ad66026ad56f03691630c48bf
antonis <antonis@antonis.com>
2021-06-09 16:33:37 +0300

    ff

--- app/src/main/java/com/example/gymius/ActivityClient.java
+++ app/src/main/java/com/example/gymius/ActivityClient.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityClient extends AppCompatActivity {
 
     String username; // to be initialized from data sent from ActivityLogin
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +18,18 @@ public class ActivityClient extends AppCompatActivity {
 
         Intent intent = getIntent();
         username = intent.getStringExtra("username");
-    }
+        button = (Button) findViewById(R.id.createSessionButton);
+        button.setOnClickListener(new View.OnClickListener(){
 
+            @Override
+            public void onClick(View v){
+                openCreateSessionActivity();
+            }
+        });
+    }
+    public void openCreateSessionActivity(){
+        Intent intent = new Intent(this, CreateSessionActivity.class);
+        startActivity(intent);
+    }
 
 }
