0b5626777f0dc3ca9733d479314911fc3dc28e2e
NeCr00 <36794189+NeCr00@users.noreply.github.com>
2021-06-11 03:03:11 +0300

    Adding group session and special session

--- app/src/main/java/com/example/gymius/ActivitySpecialSession.java
+++ app/src/main/java/com/example/gymius/ActivitySpecialSession.java
@@ -2,19 +2,13 @@ package com.example.gymius;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class ActivityGymSession extends AppCompatActivity {
-
-    String username; // to be initialized from data sent from ActivityLogin
+public class ActivitySpecialSession extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.gym_session);
-
-      
+        setContentView(R.layout.special_session);
     }
 }
-
