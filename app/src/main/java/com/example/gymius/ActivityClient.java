19f1ba3db0cba3862f681f559514aeb02a34655b
antonis <antonis@antonis.com>
2021-06-11 20:16:59 +0300

    select session screen

--- app/src/main/java/com/example/gymius/ActivityClient.java
+++ app/src/main/java/com/example/gymius/ActivityClient.java
@@ -54,6 +54,7 @@ public class ActivityClient extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent1 = new Intent(getApplicationContext(),EditSessionActivity.class);
+                startActivity(intent1);
             }
         });
 
--- app/src/main/java/com/example/gymius/EditSessionActivity.java
+++ app/src/main/java/com/example/gymius/EditSessionActivity.java
@@ -3,6 +3,8 @@ package com.example.gymius;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class EditSessionActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class EditSessionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_session);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner3);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(EditSessionActivity.this, android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.sessions));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+
     }
+
 }
