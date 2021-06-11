d00f6576986c8eacc0a2394bc859539227bc11bb
antonis <antonis@antonis.com>
2021-06-11 21:29:14 +0300

    delete session screen finished

--- app/src/main/java/com/example/gymius/DeleteSessionActivity.java
+++ app/src/main/java/com/example/gymius/DeleteSessionActivity.java
@@ -2,29 +2,40 @@ package com.example.gymius;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 public class DeleteSessionActivity extends AppCompatActivity {
 
-    ListView listView;
+    Button deleteTheSession;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete_session);
-        listView = (ListView)findViewById((R.id.listview));
 
-        ArrayList<String> arrayList = new ArrayList<>();
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner5);
 
-        arrayList.add("hope");
-        arrayList.add("its");
-        arrayList.add("working");
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(DeleteSessionActivity.this, android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.sessions));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line,arrayList);
+        deleteTheSession = findViewById(R.id.deleteTheSession);
+        deleteTheSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getApplicationContext(),DeleteSessionActivity.class);
+                startActivity(intent1);
+            }
+        });
 
-        listView.setAdapter(arrayAdapter);
+
+
     }
 }
